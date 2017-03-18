f1d0a31b88ed6217ece680fd12b291a28daf1de0
fish <andreasuranus@hotmail.com>
2017-03-18 12:31:54 +0100

    sdf

--- src/tests/testEX.java
+++ src/tests/testEX.java
@@ -31,6 +31,13 @@ public class testEX {
 		
 		
 		phiStates.clear();
+		phiStates.add(model.getState(1));
+		acceptedStates = model.EX(phiStates);
+		assertEquals(1, acceptedStates.size());
+		assertTrue(acceptedStates.contains(model.getState(2)));
+		
+		
+		phiStates.clear();
 		phiStates.add(model.getState(2));
 		acceptedStates = model.EX(phiStates);
 		assertEquals(1, acceptedStates.size());
@@ -106,6 +113,23 @@ public class testEX {
 		assertTrue(acceptedStates.contains(model.getState(2)));
 		assertTrue(acceptedStates.contains(model.getState(3)));
 		assertTrue(acceptedStates.contains(model.getState(4)));
+	}
+	
+	@Test
+	public void test2() {
+		Model model = new Model("v,c");
+		model.addState(1, "v", "");
+		model.addState(2, "v", "1,3");
+		model.addState(3, "c", "2,4");
+		model.addState(4, "c", "");
+		model.setStartStates("1,2,3,4");
+		model.initialize();
+		
+		HashSet<State> phiStates = new HashSet<State>();
+		phiStates.add(model.getState(1));
+		HashSet<State> acceptedStates = model.EX(phiStates);
+		assertEquals(1, acceptedStates.size());
+		assertTrue(acceptedStates.contains(model.getState(2)));
 	}
 
 }
